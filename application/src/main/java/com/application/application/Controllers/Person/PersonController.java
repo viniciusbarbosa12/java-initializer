0febe2967682d3da6b833da220667c3bc1a96c5d
Vinicius Barbosa <viniciusbarbosa.gt4w@gmail.com>
2024-03-22 20:01:17 -0300

    change annotation for crud

--- application/src/main/java/com/application/application/Controllers/Person/PersonController.java
+++ application/src/main/java/com/application/application/Controllers/Person/PersonController.java
@@ -17,39 +17,33 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findBydId(@PathVariable(value = "id") Long id) throws Exception {
-        return personService.findById(id);
-    }
 
-
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
 
         return personService.getAll();
     }
 
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findBydId(@PathVariable(value = "id") Long id) throws Exception {
+        return personService.findById(id);
+    }
 
-
-    @RequestMapping(method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity create(@RequestBody Person person) throws Exception {
         return ResponseEntity.ok(personService.create(person));
     }
 
 
-
-    @RequestMapping(method = RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity update(@RequestBody Person person) throws Exception {
         return ResponseEntity.ok(personService.update(person));
     }
 
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE,
+    @DeleteMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity delete(@PathVariable(value = "id") Long id) throws Exception {
         personService.delete(id);
